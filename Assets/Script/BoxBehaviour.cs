acfdd14eed688b6ddb9c22f43934a6675e46a1ff
moon8458 <hazuki.huahua@icloud.com>
2025-11-09 19:19:57 +0900

    bgm、効果音の追加と缶のバグ修正

--- Assets/Script/BoxBehaviour.cs
+++ Assets/Script/BoxBehaviour.cs
@@ -4,9 +4,18 @@ public class BoxBehavior : MonoBehaviour
 {
     public bool isExplosive = false;
     public GameObject explosionPrefab;
-
+    public AudioClip explosionSound;
+    [SerializeField, Range(0f, 1f)]
+    private float explosionSoundVolume = 1f;
+    private AudioSource audioSource;
+    private void Start()
+    {
+        // AudioSourceをこのオブジェクトにアタッチ
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (isExplosive)
         {
             if (collision.gameObject.CompareTag("floor") || collision.gameObject.CompareTag("Enemy"))
@@ -15,14 +24,17 @@ public class BoxBehavior : MonoBehaviour
                 if (explosionPrefab != null)
                 {
                     GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+                    audioSource.PlayOneShot(explosionSound, explosionSoundVolume);
                     Destroy(explosion, 1f);
                 }
                 else
                 {
                     Debug.LogWarning("explosionPrefabがInspectorで設定されていません");
                 }
-
-                Destroy(gameObject);
+                Destroy(GetComponent<SpriteRenderer>());
+                Collider2D col = GetComponent<Collider2D>();
+                if (col != null) Destroy(col);
+                Destroy(gameObject, 5f);
             }
         }
         else if (collision.gameObject.CompareTag("floor") || collision.gameObject.CompareTag("Enemy"))
