0444b665d92be119e6558a36c1b1263eab4b2bd6
25kanaizuka <rk06151312005@gmail.com>
2025-11-21 01:17:48 +0900

    ashiba消失の修正と設定関係

--- Assets/Script/ashiba1_side.cs
+++ Assets/Script/ashiba1_side.cs
@@ -5,7 +5,7 @@ public class ashiba1_side : MonoBehaviour
     void Start()
     {
         Vector2 top_scale = transform.localScale;
-        top_scale.x = top_scale.x + 0.0004f;
+        top_scale.x = top_scale.x + 0.04f;
         transform.localScale = top_scale;
     }
 }
